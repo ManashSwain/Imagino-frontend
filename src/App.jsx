0aa20d85cce6bc6a0cb1ac6293b85f625787ef5f
ManashSwain <manash0802@gmail.com>
2024-12-10 05:09:39 +0530

    removed unused tags

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css';
 import {
   createBrowserRouter,
   RouterProvider,
-  Link,
 } from "react-router-dom";
 import RootLayout from './Layout/RootLayout';
 import HomePage from './Pages/HomePage';
