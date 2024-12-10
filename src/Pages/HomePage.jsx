f35de68fd51ba327fa2cea5c08d0e9b2cc4f018f
ManashSwain <manash0802@gmail.com>
2024-12-10 17:33:09 +0530

    implemented working

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import Header from "../Components/Header"
 import Testimonials from "../Components/Testimonials"
+import Working from "../Components/Working"
 
 const HomePage = () => {
   return (
    <>
    <Header/>
    <Testimonials/>
+   <Working/>
    </>
   )
 }
