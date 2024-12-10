d1246d4b56506622d28562a854d1564544435ff6
ManashSwain <manash0802@gmail.com>
2024-12-10 21:39:29 +0530

    added sign up ui

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import Description from "../Components/Description"
 import Header from "../Components/Header"
+import SignUp from "../Components/SignUp"
 import Testimonials from "../Components/Testimonials"
 import Working from "../Components/Working"
 
