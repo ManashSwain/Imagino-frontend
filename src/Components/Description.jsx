c1c088c7ac6b4c17866ec4e4aaa2641a1f64f64c
ManashSwain <manash0802@gmail.com>
2024-12-21 15:16:54 +0530

    implemented animations in description comp

--- src/Components/Description.jsx
+++ src/Components/Description.jsx
@@ -15,14 +15,14 @@ const Description = () => {
   </motion.div>
     <div className="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
       <motion.img
-      initial={{opacity:0.4 , x:-100 }}
-      transition={{ duration:1.5}}
-      whileInView={{opacity : 1 , x:0}} alt="feature" className="object-cover object-center h-96 w-full" src="spaceship.jpg"/>
+      initial={{opacity:0.4 , y:200 }}
+      transition={{ duration:1}}
+      whileInView={{opacity : 1 , y:0}} alt="feature" className="object-cover object-center h-96 w-full" src="spaceship.jpg"/>
     </div>
     <motion.div
-     initial={{opacity:0.4 , x: 100 }}
-     transition={{ duration:1.5}}
-     whileInView={{opacity : 1 , x:0}} className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center"> 
+     initial={{opacity:0.4 , y: 200 }}
+     transition={{ duration:1}}
+     whileInView={{opacity : 1 , y:0}} className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center"> 
       <h2 className="text-2xl font-bold mb-8">Transform Words into Stunning Visuals with Ease!</h2>
       <p className="text-xl font-medium text-gray-400">Unleash the power of AI to turn your text into breathtaking images in just a few clicks. Whether you are a designer, content creator, or simply someone with a creative spark, our app lets you bring your ideas to life effortlessly. From conceptual art to realistic scenes, type your imagination and let the magic happen. No design skills needed—just your words and our AI to create limitless possibilities. Experience creativity redefined!</p>
       
