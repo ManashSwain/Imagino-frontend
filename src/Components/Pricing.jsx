3b20b341e86adc203079a4dbc36d53566af08a76
ManashSwain <manash0802@gmail.com>
2024-12-21 15:31:15 +0530

    added animations in pricing sections

--- src/Components/Pricing.jsx
+++ src/Components/Pricing.jsx
@@ -1,5 +1,6 @@
-import { useContext } from "react"
-import { Appcontext } from "../Context/ContextProvider"
+import { useContext } from "react";
+import { Appcontext } from "../Context/ContextProvider";
+import { motion } from "motion/react" ;
 
 const Pricing = () => {
   const {user} = useContext(Appcontext)
@@ -9,13 +10,22 @@ const Pricing = () => {
   <div className="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl" aria-hidden="true">
     <div className="mx-auto aspect-[1155/678] w-[72.1875rem]  opacity-30" ></div>
   </div>
-  <div className="mx-auto max-w-4xl text-center">
+  <motion.div
+   initial={{opacity:0.3 , y:100}}
+   transition={{ duration :0.5}}
+   whileInView={{opacity : 1 , y :0}} className="mx-auto max-w-4xl text-center">
     <h2 className="text-base/7 font-semibold text-indigo-600">Pricing</h2>
     <p className="mt-2 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-6xl ">Choose the right plan for you</p>
-  </div>
-  <p className="mx-auto mt-6 max-w-2xl text-pretty text-center text-lg font-medium text-gray-600 sm:text-xl/8">Choose an affordable plan that’s packed with the best benefits for you .</p>
+  </motion.div>
+  <motion.p
+   initial={{opacity:0.3 , y:100}}
+   transition={{ duration :0.5}}
+   whileInView={{opacity : 1 , y :0}} className="mx-auto mt-6 max-w-2xl text-pretty text-center text-lg font-medium text-gray-600 sm:text-xl/8">Choose an affordable plan that’s packed with the best benefits for you .</motion.p>
   <div className="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2">
-    <div className="rounded-3xl rounded-t-3xl bg-white/60 p-8 ring-1 ring-gray-900/10 sm:mx-8 sm:rounded-b-none sm:p-10 lg:mx-0 lg:rounded-bl-3xl lg:rounded-tr-none">
+    <motion.div
+    initial={{opacity:0.3 , y:100}}
+    transition={{ delay : 0.2 , duration :0.5}}
+    whileInView={{opacity : 1 , y :0}} className="rounded-3xl rounded-t-3xl bg-white/60 p-8 ring-1 ring-gray-900/10 sm:mx-8 sm:rounded-b-none sm:p-10 lg:mx-0 lg:rounded-bl-3xl lg:rounded-tr-none">
       <h3 id="tier-hobby" className="text-base/7 font-semibold text-indigo-600">Basic</h3>
       <p className="mt-4 flex items-baseline gap-x-2">
         <span className="text-5xl font-semibold tracking-tight text-gray-900">₹50</span>
@@ -49,8 +59,11 @@ const Pricing = () => {
         </li>
       </ul>
       <a href="#" aria-describedby="tier-hobby" className="mt-8 block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:mt-10">{user ? "Purchase" : "Get started today"  }</a>
-    </div>
-    <div className="relative rounded-3xl bg-gray-900 p-8 shadow-2xl ring-1 ring-gray-900/10 sm:p-10">
+    </motion.div>
+    <motion.div
+    initial={{opacity:0.3 , y:100}}
+    transition={{ delay : 0.2 ,duration :0.5}}
+    whileInView={{ opacity : 1 , y :0}} className="relative rounded-3xl bg-gray-900 p-8 shadow-2xl ring-1 ring-gray-900/10 sm:p-10">
       <h3 id="tier-enterprise" className="text-base/7 font-semibold text-indigo-400">Advanced</h3>
       <p className="mt-4 flex items-baseline gap-x-2">
         <span className="text-5xl font-semibold tracking-tight text-white">₹100</span>
@@ -91,7 +104,7 @@ const Pricing = () => {
        
       </ul>
       <a href="#" aria-describedby="tier-enterprise" className="mt-8 block rounded-md bg-indigo-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 sm:mt-10">{user ? "Purchase" : "Get started today"  }</a>
-    </div>
+    </motion.div>
   </div>
 </div>
 
