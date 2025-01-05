d4633d6fed968ede044f287dc34cadbc992a5762
ManashSwain <manash0802@gmail.com>
2025-01-05 22:34:57 +0530

    minor tweeks

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -10,7 +10,7 @@ const Error = () => {
     <h1 className="mt-4 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">Page not found</h1>
     <p className="mt-6 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">Sorry, we couldn’t find the page you’re looking for.</p>
     <div className="mt-10 flex items-center justify-center gap-x-6">
-      <span onClick={()=>{navigate('/')}} className=" cursor-pointer rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go back home</span>
+      <span onClick={()=>{navigate('/')}} className=" cursor-pointer rounded-md bg-indigo-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go back home</span>
     </div>
   </div>
 </main>
